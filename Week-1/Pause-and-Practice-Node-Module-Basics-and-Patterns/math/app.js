09f49245fa186eda29102258a290029ed9dc9be4
Darth-Revan-13 <alexander.hilbert@bryanuniversity.edu>
2020-09-25 11:36:31 -0400

    week 1

---
+++ Week-1/Pause-and-Practice-Node-Module-Basics-and-Patterns/math/app.js
@@ -0,0 +1,6 @@
+const math = require("./math.js");
+
+console.log("Addition: " + math.add(5, 8));
+console.log("Subtraction: " + math.sub(10, 2));
+console.log("Division: " + math.div(20, 2));
+console.log("Multiplication: " + math.mul(8, 4));
---
+++ Week-1/Pause-and-Practice-Node-Module-Basics-and-Patterns/math/math.js
@@ -0,0 +1,22 @@
+function add (num1, num2) {
+    return (num1 + num2)
+};
+
+function sub (num1, num2) {
+    return (num1 - num2)
+};
+
+function div (num1, num2) {
+    return (num1 / num2)
+};
+
+function mul (num1, num2) {
+    return (num1 * num2)
+};
+
+module.exports = {
+    add: add,
+    sub: sub,
+    div: div,
+    mul: mul
+}
